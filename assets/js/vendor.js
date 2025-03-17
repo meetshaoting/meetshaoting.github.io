3a7928a27f0a785ade7f9bfb05de0b8ccb3e643b
instrumaker <instrumaker@github.com>
2025-03-17 18:44:06 +0800

    merge all the js into html to reduce http requests

--- assets/js/vendor.js
+++
@@ -1,5 +0,0 @@
-// 包含所有JavaScript依赖
-document.write('<script src="/assets/js/jquery.min.js"></script>');
-document.write('<script src="/assets/js/zooming.min.js"></script>');
-document.write('<script src="/assets/js/retina.min.js"></script>');
-document.write('<script src="/assets/js/svgxuse.min.js"></script>');
