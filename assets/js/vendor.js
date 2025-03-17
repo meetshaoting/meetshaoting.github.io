5e0cebf80207acabcf5b82073d12fd64f3065600
instrumaker <instrumaker@github.com>
2025-03-17 14:39:00 +0800

    add favicon, delete imgs not using

--- assets/js/vendor.js
+++ assets/js/vendor.js
@@ -3,4 +3,3 @@ document.write('<script src="/assets/js/jquery.min.js"></script>');
 document.write('<script src="/assets/js/zooming.min.js"></script>');
 document.write('<script src="/assets/js/retina.min.js"></script>');
 document.write('<script src="/assets/js/svgxuse.min.js"></script>');
-document.write('<script src="/assets/js/webfontloader.js"></script>');
